7c3e8f1d7e7fc4b8a0539a0b9f73643ed9307619
NaimalArain13 <136015168+NaimalArain13@users.noreply.github.com>
2024-02-26 11:38:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+"use strict";
+let message = "Governor Sindh Initiative for Cloud Applied Generative AI";
+console.log(message);
---
+++ app.ts
@@ -0,0 +1,2 @@
+let message = "Governor Sindh Initiative for Cloud Applied Generative AI";
+console.log(message);
